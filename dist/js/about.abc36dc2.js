5b28481be8751c503dbab5909483c793b7b2161f
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-03-03 14:13:20 -0800

    new build

--- dist/js/about.abc36dc2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},o=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is the about page.")])])}],u={name:"about",created:function(){document.title=document.location.hostname}},c=u,s=n("2877"),i=Object(s["a"])(c,a,o,!1,null,null,null);e["default"]=i.exports}}]);
-//# sourceMappingURL=about.abc36dc2.js.map
